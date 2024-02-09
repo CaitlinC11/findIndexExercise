5451e1ecbf0f7dafa2aba071a8a687603d42404c
CaitlinC11 <chr1111@live.com>
2024-02-09 11:21:27 -0500

    answers to exercise

--- find-findindex.js
+++ find-findindex.js
@@ -12,7 +12,9 @@ findUserByUsername(users, 'taco') // undefined
 */
 
 
-function findUserByUsername(usersArray, username) {}
+function findUserByUsername(usersArray, username) {
+  return usersArray.find(user => user.username === username);
+}
 
 /*
 Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
@@ -27,4 +29,14 @@ removeUser(users, 'akagen') // {username: 'akagen'}
 removeUser(users, 'akagen') // undefined
 */
 
-function removeUser(usersArray, username) {}
+function removeUser(usersArray, username) {
+  let userIndex = usersArray.findIndex(user => user.username === username);
+  if (userIndex > -1) {
+    // Remove the user from the array
+    let [removedUser] = usersArray.splice(userIndex, 1);
+    // Return the removed user
+    return removedUser;
+  }
+  // Return undefined if user was not found
+  return undefined;
+}
